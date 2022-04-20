cb255417540f95d1d2b2610c9d9511e4178f6598
alimertcakar <mail@alimert.net>
2022-04-20 23:32:17 +0300

    .

--- index.mjs
+++ index.mjs
@@ -1,8 +1,29 @@
-var x = 1;
-function sideeffect(x) {
-  x = 10;
-  x--;
-  console.log(x, "x");
+"use strict";
+
+const not =
+  (fn) =>
+  (...args) =>
+    !fn(...args);
+
+const output = (x) => console.log(x);
+
+const when = (fn) => (predicate) => (args) =>
+  predicate(args) ? fn(args) : undefined;
+
+function isShortEnough(str) {
+  return str.length <= 5;
 }
 
-sideeffect(x);
+function isLongEnough(str) {
+  return not(isShortEnough);
+}
+
+var msg1 = "Hello";
+var msg2 = msg1 + " World";
+
+const printIf = when(console.log);
+
+printIf(isShortEnough)(msg1); // Hello
+printIf(isShortEnough)(msg2);
+printIf(isLongEnough)(msg1);
+printIf(isLongEnough)(msg2); // Hello World
