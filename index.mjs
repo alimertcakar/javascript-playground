b3a88e3e8089412fc51639ff2be6362e59a23839
alimertcakar <mail@alimert.net>
2022-04-21 10:24:46 +0300

    .

--- index.mjs
+++ index.mjs
@@ -1,29 +1,15 @@
-"use strict";
-
-const not =
-  (fn) =>
-  (...args) =>
-    !fn(...args);
-
-const output = (x) => console.log(x);
-
-const when = (fn) => (predicate) => (args) =>
-  predicate(args) ? fn(args) : undefined;
-
-function isShortEnough(str) {
-  return str.length <= 5;
+function padder(count) {
+  // const padding = "".padStart(count, "0");
+  console.log("x");
+  return () => {
+    const padding = "".padStart(count, "0");
+    console.log("y");
+    return padding;
+  };
 }
 
-function isLongEnough(str) {
-  return not(isShortEnough);
-}
+const padder5 = padder(5);
 
-var msg1 = "Hello";
-var msg2 = msg1 + " World";
-
-const printIf = when(console.log);
-
-printIf(isShortEnough)(msg1); // Hello
-printIf(isShortEnough)(msg2);
-printIf(isLongEnough)(msg1);
-printIf(isLongEnough)(msg2); // Hello World
+console.log(padder5());
+console.log(padder5());
+console.log(padder5());
