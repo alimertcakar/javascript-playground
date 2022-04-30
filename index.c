76427c7c89dfa4c564aac732e1a861b8b5bb1d3f
alimertcakar <mail@alimert.net>
2022-04-30 18:34:45 +0300

    init c

--- index.c
+++ index.c
@@ -1,10 +1,20 @@
-// c-playground.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#include <stdio.h>
 
 int main()
 {
-	printf("hello");
+	char chars[6] = "abcdef";
+
+	int i;
+	int j;
+	int len = strlen(chars);
+	for (i = 0; i < 6;i++) {
+		for (j = 0; j < 6;j++) {
+			printf("%c", chars[i]);
+			printf("%c \n", chars[j]);
+		}
+	}
+
+
+	return 0;
 }
 
