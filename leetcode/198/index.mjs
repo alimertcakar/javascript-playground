036d86e4f1b90a9bc843375766ffebbd5fea6c27
Ali Mert Çakar <mail@alimert.net>
2022-06-08 00:05:19 +0300

    code

--- leetcode/198/index.mjs
+++ leetcode/198/index.mjs
@@ -1,12 +1,20 @@
 const houses = [2, 7, 9, 3, 1];
 
+//  2,7,9,3,1
+// -7,-11,-10,-10,-3
+// 5, 4,-1,-7,-2
+
 function robHouses() {
-  let i = 0;
+  let prevprev = 0;
+  let prev = 0;
+
   for (const house of houses) {
-    const prevHouse = houses[i - 1] || 0;
-    const nextHouse = houses[i + 1] || 0;
-    i++;
+    const temp = Math.max(house + prevprev, prev);
+    prevprev = prev;
+    prev = temp;
   }
+
+  return prev;
 }
 
-robHouses();
+console.log(robHouses());
